b8f5e6aaea2a1fe754701f068cc079ecb1d77a75
Jinhyung Kim <jinhyung@whimr.net>
2016-08-17 12:55:39 +0900

    pilot cdr generator base

---
+++ pilot/src/cdrgen/cdrgen.go
@@ -0,0 +1,35 @@
+package cdrgen
+
+import (
+	"fmt"
+	"github.com/op/go-logging"
+)
+
+var log = logging.MustGetLogger("example")
+
+var format = logging.MustStringFormatter(
+	`%{color}%{time:15:04:05.000} %{shortfunc} ▶ %{level:.4s} %{id:03x}%{color:reset} %{message}`,
+)
+
+func main() {
+	backend1 := logging.NewLogBackend(os.Stderr, "", 0)
+
+	// For messages written to backend2 we want to add some additional
+	// information to the output, including the used log level and the name of
+	// the function.
+	backend2Formatter := logging.NewBackendFormatter(backend2, format)
+
+	// Only errors and more severe messages should be sent to backend1
+	backend1Leveled := logging.AddModuleLevel(backend1)
+	backend1Leveled.SetLevel(logging.ERROR, "")
+
+	// Set the backends to be used.
+	logging.SetBackend(backend1Leveled, backend2Formatter)
+
+	log.Debugf("debug %s", Password("secret"))
+	log.Info("info")
+	log.Notice("notice")
+	log.Warning("warning")
+	log.Error("err")
+	log.Critical("crit")
+}
